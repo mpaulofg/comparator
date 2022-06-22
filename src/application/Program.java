e636f61102e62d3feaea2aa70613866f4319f541
mpaulofg <mpaulofg@gmail.com>
2022-06-22 14:47:33 -0300

    first version

---
+++ src/application/Program.java
@@ -0,0 +1,26 @@
+package application;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+import entities.Product;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		List<Product> list = new ArrayList<>();
+		list.add(new Product("TV", 900.0));
+		list.add(new Product("Notebook", 1200.0));
+		list.add(new Product("Tablet", 400.0));
+		
+		Collections.sort(list);
+		
+		for(Product p : list ) {
+			System.out.println(p);
+		}
+
+	}
+
+}
